2e9e1582f2adc393d083b17e87df97fbeb999af5
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-03-31 15:44:46 -0400

    Prep to embed into main website

---
+++ public/pymSetup.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  const pymParent = new pym.Parent(
+    "cares-act-calculator",
+    "https://cares-act-calculator.netlify.com//index.html",
+    {}
+  );
+});
